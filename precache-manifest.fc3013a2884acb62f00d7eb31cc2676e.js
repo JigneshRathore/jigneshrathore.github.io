680187d5a04cd0491b2229663a659cf4b9205997
Jignesh Rathod <44917589+JigneshRathore@users.noreply.github.com>
2021-09-16 18:00:22 +0530

    filed added

---
+++ precache-manifest.fc3013a2884acb62f00d7eb31cc2676e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46c062543f6dd791c9e61481b3cd118",
+    "url": "/beingdevelopers/jignesh.github.io/index.html"
+  },
+  {
+    "revision": "d9b0ae1a915bd1a0a249",
+    "url": "/beingdevelopers/jignesh.github.io/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "4081edd3b2a15f284cb6",
+    "url": "/beingdevelopers/jignesh.github.io/static/css/main.cce32172.chunk.css"
+  },
+  {
+    "revision": "d9b0ae1a915bd1a0a249",
+    "url": "/beingdevelopers/jignesh.github.io/static/js/2.82a08f41.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/beingdevelopers/jignesh.github.io/static/js/2.82a08f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4081edd3b2a15f284cb6",
+    "url": "/beingdevelopers/jignesh.github.io/static/js/main.701259e5.chunk.js"
+  },
+  {
+    "revision": "dca647c432617788b417",
+    "url": "/beingdevelopers/jignesh.github.io/static/js/runtime-main.f459dea6.js"
+  }
+]);
